44969d9cc6bdefdc99778e478dfa5f4ebb16f10a
Matthew Seccafien <matt@studiocartogram.com>
2014-11-30 20:22:12 +0700

    refactored gulp process

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,9 +19,9 @@ module.exports = function (grunt) {
     // Add shopify theme url here
     var config = {
         app: 'app',
-        theme:'bikes-on-wheels',
-        dist: 'bikes-on-wheels/assets',
-        snippets: '../bikes-on-wheels/snippets'
+        theme:'sell-in-may',
+        dist: 'sell-in-may/assets',
+        snippets: '../sell-in-may/snippets'
     };
 
     // Define the configuration for all the tasks
@@ -46,21 +46,12 @@ module.exports = function (grunt) {
 
 
         shopify: {
-            //Staging
-            // options: {
-            //     api_key: 'ef395c6764a48a68887522090392b090',
-            //     password: '0ccdd74708ab1b4c1b0bec0348cc6700',
-            //     url: 'bikes-on-wheels-2.myshopify.com',
-            //     base: 'bikes-on-wheels/'
-            // }
-            // ,
-            //Production
             options: {
-                api_key: '0150d3583f94e054b4cd1e53cadee0f8',
-                password: '5a6dce46e094250440715196049e721e',
-                url: 'bikes-on-wheels-3.myshopify.com',
-                base: 'bikes-on-wheels/',
-                theme: '10963799'
+                api_key: '0c7edac103cecdb0bf26c8305841a199',
+                password: 'f7d4445540967af654f78d13d594eedc',
+                url: 'sell-in-may.myshopify.com',
+                base: 'sell-in-may/'
+            //    theme: '10963799'
             }
         },
     });
--- gulp/build.js
+++ gulp/build.js
@@ -3,16 +3,28 @@
 var gulp = require('gulp');
 
 var $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license']
+    pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
 });
 
-var paths = {
-    theme : 'bikes-on-wheels',
-    scripts : [
-
-    ]
-
-}
+var theme = 'sell-in-may',
+    dist = theme + '/assets',
+    source = 'src',
+    bower = source + '/bower_components',
+    scripts = [
+        bower + '/lodash/dist/lodash.js',
+        bower + '/angular-waypoints/dist/angular-waypoints.all.js',
+        bower + '/jquery.lazyload/jquery.lazyload.js',
+        bower + '/swiper/idangerous.swiper.js',
+        source + '/scripts/cartogram.background.js',
+        source + '/scripts/cartogram.dimensions.js',
+        source + '/scripts/cartogram.fill.js',
+        source + '/scripts/cartogram.events.js',
+        source + '/scripts/cartogram.toggle.js',
+        source + '/scripts/cartogram.scroll.js',
+        source + '/scripts/cartogram.loadWatch.js',
+        source + '/scripts/cart.js',
+        source + '/scripts/script.js'
+    ];
 
 function handleError(err) {
     console.error(err.toString());
@@ -20,63 +32,49 @@ function handleError(err) {
 }
 
 gulp.task('styles', function () {
-    return gulp.src('src/styles/**/*.scss')
-        .pipe($.sourcemaps.init())
-        .pipe($.sass({style: 'expanded', includePaths: ['src/bower_components/foundation/scss']}))
-        .on('error', handleError)
-        .pipe($.sourcemaps.write())
+    return gulp.src(source + '/styles/**/*.scss')
+        .pipe($.sass({style: 'expanded', includePaths: [bower + '/foundation/scss']}))
+        .on('error', function handleError(err) {
+            console.error(err.toString());
+            this.emit('end');
+        })
         .pipe($.autoprefixer('last 1 version'))
         .pipe($.csso())
         .pipe($.rename(function (path) {
             path.extname = ".css.liquid"
         }))
-        .pipe(gulp.dest(paths.theme + '/assets'))
+        .pipe(gulp.dest(dist))
         .pipe($.size());
 });
 
 gulp.task('scripts', function () {
-    return gulp.src([
-        'src/bower_components/lodash/dist/lodash.js',
-        'src/bower_components/angular-waypoints/dist/angular-waypoints.all.js',
-        'src/bower_components/jquery.lazyload/jquery.lazyload.js',
-        'src/bower_components/swiper/src/idangerous.swiper.js',
-        'src/bower_components/jquery-instagram/dist/instagram.js',
-        'src/scripts/cartogram.background.js',
-        'src/scripts/cartogram.dimensions.js',
-        'src/scripts/cartogram.fill.js',
-        'src/scripts/cartogram.events.js',
-        'src/scripts/cartogram.toggle.js',
-        'src/scripts/cartogram.scroll.js',
-        'src/scripts/cartogram.loadWatch.js',
-        'src/scripts/cart.js',
-        'src/scripts/script.js'
-        ])
+    return gulp.src(scripts)
         .pipe($.cached('scripts'))
         .pipe($.jshint())
         .pipe($.jshint.reporter('jshint-stylish'))
         .pipe($.remember('scripts'))
         .pipe($.concat('scripts.js.liquid'))
         .pipe($.ngAnnotate())
-        .pipe($.uglify({preserveComments: $.uglifySaveLicense}))
-        .pipe(gulp.dest('bikes-on-wheels/assets'))
+        .pipe($.uglify())
+        .pipe(gulp.dest(dist))
         .pipe($.size());
 });
 
 
 gulp.task('images', function () {
-    return gulp.src('src/images/*.{png,jpg,gif}')
+    return gulp.src(source + '/images/*.{png,jpg,gif}')
     .pipe($.cache($.imagemin({
         optimizationLevel: 3,
         progressive: true,
         interlaced: true
     })))
     .pipe($.flatten())
-    .pipe(gulp.dest('bikes-on-wheels/assets'))
+    .pipe(gulp.dest(dist))
     .pipe($.size());
 });
 
 gulp.task('svgs', function () {
-    return gulp.src('src/images/svgs/*.svg')
+    return gulp.src(source + '/images/svgs/*.svg')
         .pipe($.svgSprites({
             mode: "symbols",
             svg: {
@@ -85,11 +83,20 @@ gulp.task('svgs', function () {
             },
             preview: false
         }))
-        .pipe(gulp.dest("bikes-on-wheels"));
+        .pipe(gulp.dest(theme));
 });
 
-gulp.task('clean', function () {
-    return gulp.src(['.tmp', 'bikes-on-wheels/assets'], { read: false }).pipe($.rimraf());
+
+gulp.task('fonts', function () {
+    return gulp.src(source + '/fonts/**/*.{eot,svg,ttf,woff}')
+    .pipe($.flatten())
+    .pipe(gulp.dest(dist))
+    .pipe($.size());
 });
 
-gulp.task('build', ['clean', 'scripts', 'styles', 'images', 'svgs']);
+
+gulp.task('clean', function (done) {
+    $.del([dist, '.tmp/'], done);
+});
+
+gulp.task('build', ['clean', 'scripts', 'styles', 'images', 'svgs', 'fonts']);
